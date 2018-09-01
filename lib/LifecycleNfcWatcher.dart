6e12809b66ee81b9d118a3692923acc67e2296f3
OrMantzur <or.mantzur@gmail.com>
2018-09-01 20:48:00 +0300

    open app by NFC(when the app on device close)

--- lib/LifecycleNfcWatcher.dart
+++ lib/LifecycleNfcWatcher.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 class LifecycleNfcWatcher extends StatefulWidget {
+
   @override
   _LifecycleNfcWatcherState createState() => _LifecycleNfcWatcherState();
 }
